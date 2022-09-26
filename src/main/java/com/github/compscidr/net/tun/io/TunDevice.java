7cee217f4858fbcf15ce5284211eea88a73b15d5
Jason Ernst <ernstjason1@gmail.com>
2022-09-26 16:26:39 -0700

    still working on the byte ordering

--- src/main/java/com/github/compscidr/net/tun/io/TunDevice.java
+++ src/main/java/com/github/compscidr/net/tun/io/TunDevice.java
@@ -167,6 +167,7 @@ public class TunDevice implements AutoCloseable {
 			Thread.sleep(10);
 		}
 		ByteBuffer packet = ByteBuffer.allocate(availableForRead);
+		packet.order(ByteOrder.BIG_ENDIAN);
 		packet.put(inbuf);
 		packet.rewind();
 		availableForRead = 0;
